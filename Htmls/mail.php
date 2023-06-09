9d158d8bef8f852e940bdaf46f1df4afa32008e6
Abhinandan <coolaj2712@gmail.com>
2023-06-09 14:16:52 +0530

    initial commit

---
+++ Htmls/mail.php
@@ -0,0 +1,19 @@
+<?php
+//get data from form  
+
+$name = $_POST['name'];
+$email= $_POST['email'];
+$subject= $_POST['subject'];
+$message= $_POST['message'];
+
+$to = "coolaj2712@gmail.com";
+$subject = "Mail From music website contact form";
+$txt ="Name = ". $name . "\r\n  Email = " . $email ."\r\n  subject = " . $subject . "\r\n Message =" . $message;
+$headers = "From: noreply@yoursite.com" . "\r\n" .
+"CC: somebodyelse@example.com";
+if($email!=NULL){
+    mail($to,$subject,$txt,$headers);
+}
+//redirect
+header("thankyou.html");
+?>
